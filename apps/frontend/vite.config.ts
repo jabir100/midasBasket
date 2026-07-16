90e4ac426382d03aa511df7948b935539e04992b
habib-153 <h.r.sihab155@gmail.com>
2026-07-16 23:19:08 +0600

    frontend dist

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,11 +1,12 @@
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
+import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   server: {
     port: 3000,
   },
-  plugins: [tanstackStart(), tailwindcss(), viteReact()],
+  plugins: [tanstackStart(), nitro(), tailwindcss(), viteReact()],
 });
